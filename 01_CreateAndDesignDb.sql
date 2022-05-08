93ed94c38baf71cb196d420da4939ede742d0efb
kaloyan.ivanov <kaloyan.ivanov88@gmail.com>
2022-05-08 16:44:46 +0300

    refactored design script

--- 01_CreateAndDesignDb.sql
+++ 01_CreateAndDesignDb.sql
@@ -1,140 +1,156 @@
-CREATE DATABASE IF NOT EXISTS Restaurant;
-USE Restaurant;
+CREATE DATABASE IF NOT EXISTS restaurant;
+USE restaurant;
 -- create roles----------------
-CREATE TABLE Roles (
-    Id INT AUTO_INCREMENT,
-    RoleType VARCHAR(30) NOT NULL,
-    IsDeleted BIT NOT NULL DEFAULT 0,
-    CreateDate DATETIME,
-    UpdateDate DATETIME,
-    PRIMARY KEY (Id)
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT,
+    role_type VARCHAR(30) NOT NULL,
+    is_deleted BIT NOT NULL DEFAULT 0,
+    create_date DATETIME,
+    update_date DATETIME,
+    PRIMARY KEY (id)
 );
 -- create users--------------------
-CREATE TABLE Users (
-    Id INT AUTO_INCREMENT,
-    FirstName VARCHAR(20) NOT NULL,
-    LastName VARCHAR(20) NOT NULL,
-    Email VARCHAR(30),
-    `Role` INT,
-    IsDeleted BIT NOT NULL DEFAULT 0,
-    CreateDate DATETIME,
-    UpdateDate DATETIME,
-    PRIMARY KEY (Id),
-    FOREIGN KEY (`Role`)
-        REFERENCES Roles (Id)
+CREATE TABLE users (
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    email VARCHAR(30),
+    user_role INT,
+    is_deleted BIT NOT NULL DEFAULT 0,
+    create_date DATETIME,
+    update_date DATETIME,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_role)
+        REFERENCES roles (id)
 );
 -- create tables--------------------
-CREATE TABLE Tables (
-    Id INT NOT NULL AUTO_INCREMENT,
-    Capacity INT NOT NULL,
-    IsDeleted BIT NOT NULL DEFAULT 0,
-    CreateDate DATETIME,
-    UpdateDate DATETIME,
-    CreatedBy INT,
-    UpdatedBy INT,
-    PRIMARY KEY (Id),
-    FOREIGN KEY (CreatedBy)
-        REFERENCES Users (Id),
-    FOREIGN KEY (UpdatedBy)
-        REFERENCES Users (Id)
+CREATE TABLE restaurant_tables (
+    id INT NOT NULL AUTO_INCREMENT,
+    capacity INT NOT NULL,
+    is_deleted BIT NOT NULL DEFAULT 0,
+    create_date DATETIME,
+    update_date DATETIME,
+    created_by INT,
+    updated_by INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (created_by)
+        REFERENCES users (id),
+    FOREIGN KEY (updated_by)
+        REFERENCES users (id)
 );
 -- create ProductCategories
-CREATE TABLE ProductCategories (
-    Id INT AUTO_INCREMENT,
-    `Name` VARCHAR(30) NOT NULL,
-    IsDeleted BIT NOT NULL DEFAULT 0,
-    CreateDate DATETIME,
-    UpdateDate DATETIME,
-    CreatedBy INT,
-    UpdatedBy INT,
-    PRIMARY KEY (Id),
-    FOREIGN KEY (CreatedBy)
-        REFERENCES Users (Id),
-    FOREIGN KEY (UpdatedBy)
-        REFERENCES Users (Id)
+CREATE TABLE product_categories (
+    id INT AUTO_INCREMENT,
+    product_category_name VARCHAR(30) NOT NULL,
+    is_deleted BIT NOT NULL DEFAULT 0,
+    create_date DATETIME,
+    update_date DATETIME,
+    created_by INT,
+    updated_by INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (created_by)
+        REFERENCES users (id),
+    FOREIGN KEY (updated_by)
+        REFERENCES users (id)
 );
 -- create ProductTypes
-CREATE TABLE CategoryTypes (
-    Id INT AUTO_INCREMENT,
-    `MainCategory` INT REFERENCES ProductCategories (Id),
-    `Name` VARCHAR(30) NOT NULL,
-    IsDeleted BIT NOT NULL DEFAULT 0,
-    CreateDate DATETIME,
-    UpdateDate DATETIME,
-    CreatedBy INT,
-    UpdatedBy INT,
-    PRIMARY KEY (Id),
-    FOREIGN KEY (CreatedBy)
-        REFERENCES Users (Id),
-    FOREIGN KEY (UpdatedBy)
-        REFERENCES Users (Id)
+CREATE TABLE category_types (
+    id INT AUTO_INCREMENT,
+    main_category INT,
+    category_type_name VARCHAR(30) NOT NULL,
+    is_deleted BIT NOT NULL DEFAULT 0,
+    create_date DATETIME,
+    update_date DATETIME,
+    created_by INT,
+    updated_by INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (main_category)
+		REFERENCES product_categories (id),
+    FOREIGN KEY (created_by)
+        REFERENCES users (id),
+    FOREIGN KEY (updated_by)
+        REFERENCES users (id)
 );
 -- create products
-CREATE TABLE Products (
-    Id INT AUTO_INCREMENT,
-    `Name` VARCHAR(40) NOT NULL,
-    Category INT REFERENCES ProductCategories (Id),
-    `Type` INT REFERENCES CategoryTypes (Id),
-    ImagePath BLOB,
-    Price DECIMAL NOT NULL,
-    CHECK (Price > 0),
-    IsDeleted BIT NOT NULL DEFAULT 0,
-    CreateDate DATETIME,
-    UpdateDate DATETIME,
-    CreatedBy INT,
-    UpdatedBy INT,
-    PRIMARY KEY (Id),
-    FOREIGN KEY (CreatedBy)
-        REFERENCES Users (Id),
-    FOREIGN KEY (UpdatedBy)
-        REFERENCES Users (Id)
+CREATE TABLE products (
+    id INT AUTO_INCREMENT,
+    product_name VARCHAR(40) NOT NULL,
+    category INT,
+    product_type INT,
+    image_path BLOB,
+    price DECIMAL NOT NULL,
+    CHECK (price > 0),
+    is_deleted BIT NOT NULL DEFAULT 0,
+    create_date DATETIME,
+    update_date DATETIME,
+    created_by INT,
+    updated_by INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (category)
+        REFERENCES product_categories (id),
+    FOREIGN KEY (product_type)
+        REFERENCES category_types (id),
+    FOREIGN KEY (created_by)
+        REFERENCES users (id),
+    FOREIGN KEY (updated_by)
+        REFERENCES users (id)
 );
 -- create statusCodes
-CREATE TABLE OrderStatuses (
-    Id INT AUTO_INCREMENT,
-    `Name` VARCHAR(15) NOT NULL,
-    IsDeleted BIT NOT NULL DEFAULT 0,
-    CreateDate DATETIME,
-    UpdateDate DATETIME,
-    CreatedBy INT,
-    UpdatedBy INT,
-    PRIMARY KEY (Id),
-    FOREIGN KEY (CreatedBy)
-        REFERENCES Users (Id),
-    FOREIGN KEY (UpdatedBy)
-        REFERENCES Users (Id)
+CREATE TABLE order_statuses (
+    id INT AUTO_INCREMENT,
+    status_name VARCHAR(15) NOT NULL,
+    is_deleted BIT NOT NULL DEFAULT 0,
+    create_date DATETIME,
+    update_date DATETIME,
+    created_by INT,
+    updated_by INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (created_by)
+        REFERENCES users (id),
+    FOREIGN KEY (updated_by)
+        REFERENCES users (id)
 );
 -- create orders
-CREATE TABLE Orders (
-    Id INT AUTO_INCREMENT,
-    Waiter INT REFERENCES Users (Id),
-    `Table` INT REFERENCES `Tables` (Id),
-    `Status` INT REFERENCES OrderStatuses (ID),
-    IsDeleted BIT NOT NULL DEFAULT 0,
-    CreateDate DATETIME,
-    EndDate DATETIME,
-    CreatedBy INT,
-    UpdatedBy INT,
-    PRIMARY KEY (Id),
-    FOREIGN KEY (CreatedBy)
-        REFERENCES Users (Id),
-    FOREIGN KEY (UpdatedBy)
-        REFERENCES Users (Id)
+CREATE TABLE orders (
+    id INT AUTO_INCREMENT,
+    waiter INT,
+    order_table INT,
+    order_status INT,
+    is_deleted BIT NOT NULL DEFAULT 0,
+    create_date DATETIME,
+    end_date DATETIME,
+    created_by INT,
+    updated_by INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (waiter)
+		REFERENCES users (id),
+    FOREIGN KEY (order_table)
+        REFERENCES restaurant_tables (id),
+    FOREIGN KEY (order_status)
+        REFERENCES order_statuses (id),
+    FOREIGN KEY (created_by)
+        REFERENCES users (id),
+    FOREIGN KEY (updated_by)
+        REFERENCES users (id)
 );
 -- create ordersProducts
-CREATE TABLE OrdersProducts (
-    OrderId INT REFERENCES Orders (Id),
-    ProductId INT REFERENCES Products (Id),
-    ProductPrice DECIMAL,
-    ProductQuantity INT NOT NULL,
-    IsDeleted BIT NOT NULL DEFAULT 0,
-    CreateDate DATETIME,
-    UpdateDate DATETIME,
-    CreatedBy INT,
-    UpdatedBy INT,
-	FOREIGN KEY (CreatedBy)
-        REFERENCES Users (Id),
-    FOREIGN KEY (UpdatedBy)
-        REFERENCES Users (Id),
-    CONSTRAINT PK_OrdersProducts PRIMARY KEY (OrderId , ProductId)
+CREATE TABLE orders_products (
+    order_id INT,
+    product_id INT,
+    product_price DECIMAL,
+    product_quantity INT NOT NULL,
+    is_deleted BIT NOT NULL DEFAULT 0,
+    create_date DATETIME,
+    update_date DATETIME,
+    created_by INT,
+    updated_by INT,
+    FOREIGN KEY (order_id)
+        REFERENCES orders (id),
+    FOREIGN KEY (product_id)
+        REFERENCES products (id),
+    FOREIGN KEY (created_by)
+        REFERENCES users (id),
+    FOREIGN KEY (updated_by)
+        REFERENCES users (id),
+    CONSTRAINT pk_orders_products PRIMARY KEY (order_id , product_id)
 );
